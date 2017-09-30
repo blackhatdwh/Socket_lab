35f841ad4b040e61c2f840982fd5569a96dfed98
Weihao <blackhatdwh@gmail.com>
2017-09-30 11:10:48 +0800

    client in developing

--- client.c
+++ client.c
@@ -7,84 +7,110 @@
 extern int errno;
 char* server_ip = "127.0.0.1";      // define the server ip address
 int sockfd;
+int connected = 0;
+pthread_t thread_id;
 
 
-char recv_buffer[MAX_LENGTH];
 
 void Disconnect(int recv_fd){
     printf("Server disconnected!\n");
+    connected = 0;
+    close(sockfd);
+    pthread_cancel(thread_id);
 }
 
-int main(int argc, char *argv[]) {	
-    struct sockaddr_in serverAddr;
-
-    // 创建一个socket：
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        printf("socket() failed! code:%d\n", errno);
-        return -1;
-    }
-
-    // 设置服务器的地址信息：
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(SERVER_PORT);
-    serverAddr.sin_addr.s_addr = inet_addr(server_ip);
-    bzero(&(serverAddr.sin_zero), 8);
-
-    //客户端发出连接请求：
-    printf("connecting!\n");
-    if(connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-        printf("connect() failed! code:%d\n", errno);
-        close(sockfd);
-        return -1;
-    }
-
-    printf("Connected!\n");
+void* SubProcess(void* args){
+    pthread_detach(pthread_self());
+    char recv_buffer[MAX_LENGTH];
     while(1){
-        printf("Choose a function:\n");
-        printf("1.get time\t2.get name\n3.get list\t4.send msg\n");
-        int choice = 0;
-        scanf("%d", &choice);
-
-        // get time
-        if(choice == 1){
-            if(send(sockfd, function_code[0], sizeof(function_code[0]), 0) == -1) {
-                printf("send() failed!\n");
-                close(sockfd);
-                return -1;
-            }
-        }
-
-        // get name
-        if(choice == 2){
-            if(send(sockfd, function_code[1], sizeof(function_code[1]), 0) == -1) {
-                printf("send() failed!\n");
-                close(sockfd);
-                return -1;
-            }
-        }
-
-        // get list
-        if(choice == 3){
-            if(send(sockfd, function_code[2], sizeof(function_code[2]), 0) == -1) {
-                printf("send() failed!\n");
-                close(sockfd);
-                return -1;
-            }
-        }
-
-        // send msg
-        if(choice == 4){
-            if(send(sockfd, function_code[3], sizeof(function_code[3]), 0) == -1) {
-                printf("send() failed!\n");
-                close(sockfd);
-                return -1;
-            }
-        }
         Reset(recv_buffer);
         int tmp_arr[] = {sockfd};
         ReceiveData(recv_buffer, sockfd, tmp_arr, 1);
         printf("%s\n", recv_buffer);
         Reset(recv_buffer);
+    }
+}
+
+void Connect(){
+    char server_ip[20];
+    int server_port;
+    printf("Please specify server's IP and port.\n");
+    printf("IP:");
+    scanf("%s", server_ip);
+    printf("\nport:");
+    scanf("%d", &server_port);
+    struct sockaddr_in serverAddr;
+
+    // create a socket
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("socket() failed! code:%d\n", errno);
+        return;
+    }
+
+    // set up server's information
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(server_port);
+    serverAddr.sin_addr.s_addr = inet_addr(server_ip);
+    bzero(&(serverAddr.sin_zero), 8);
+
+    // send connection request to server
+    printf("connecting!\n");
+    if(connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        printf("connect() failed! code:%d\n", errno);
+        close(sockfd);
+        return;
+    }
+
+    printf("Connected!\n");
+    connected = 1;
+    int rc = pthread_create(&thread_id, NULL, SubProcess, NULL);
+    if (rc){
+        printf("ERROR; return code from pthread_create() is %d\n", rc);
+        exit(-1);
+    }
+}
+
+int main(int argc, char *argv[]) {	
+    while(1){
+        printf("Choose a function:\n");
+        printf("1.connect\n2.disconnect\n3.get time\n4.get name\n5.get list\n6.send msg\n7.exit\n");
+        int choice = 0;
+        scanf("%d", &choice);
+        // connect
+        if(choice == 1){
+            Connect();
+        }
+
+        // disconnect
+        if(choice == 2){
+            Disconnect(sockfd);
+        }
+
+
+        // get time
+        if(choice == 3){
+            SendData(sockfd, function_code[0]);
+        }
+
+        // get name
+        if(choice == 4){
+            SendData(sockfd, function_code[1]);
+        }
+
+        // get list
+        if(choice == 5){
+            SendData(sockfd, function_code[2]);
+        }
+
+        // send msg
+        if(choice == 6){
+            SendData(sockfd, function_code[3]);
+        }
+
+        // exit
+        if(choice == 7){
+
+        }
     }
 
     close(sockfd);//关闭套接字
